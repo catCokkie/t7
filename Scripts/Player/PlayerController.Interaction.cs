d5cd603646bd0d24c6ef15f00c1f720a2d11003c
catCokkie <34226396+catCokkie@users.noreply.github.com>
2025-10-22 08:11:33 +0800

    refine player interaction flow

--- Scripts/Player/PlayerController.Interaction.cs
+++ Scripts/Player/PlayerController.Interaction.cs
@@ -8,18 +8,70 @@ namespace SilentTestimony.Player
     public partial class PlayerController
     {
         [ExportGroup("Interaction")]
-        [Export(PropertyHint.Range, "8,256,1")] private float _interactRange = 64.0f;
+        private float _interactRange = 64.0f;
+
+        [Export(PropertyHint.Range, "8,256,1")]
+        private float InteractRange
+        {
+            get => _interactRange;
+            set
+            {
+                if (Mathf.IsEqualApprox(_interactRange, value))
+                    return;
+
+                _interactRange = value;
+                ApplyInteractRangeToShape();
+            }
+        }
+
         private readonly List<IInteractable> _nearbyInteractables = new();
+        private Area2D _interactorArea;
+        private CollisionShape2D _interactorShape;
+        private InteractionPrompt _interactionPrompt;
+        private IInteractable _currentInteractTarget;
+
+        partial void InitializeInteraction()
+        {
+            _interactionPrompt = GetNodeOrNull<InteractionPrompt>("InteractionPrompt");
+            _interactorArea = GetNodeOrNull<Area2D>("Interactor");
+            _interactorShape = _interactorArea?.GetNodeOrNull<CollisionShape2D>("CollisionShape2D");
+
+            ApplyInteractRangeToShape();
+            RefreshInteractTarget(force: true);
+        }
+
+        partial void UpdateInteraction(double delta)
+        {
+            RefreshInteractTarget();
+        }
 
         public override void _UnhandledInput(InputEvent @event)
         {
-            if (Input.IsActionJustPressed("interact"))
+            if (@event.IsActionPressed("interact"))
             {
                 var target = GetCurrentInteractTarget();
+                bool handled = false;
+
                 if (target != null)
                 {
                     target.Interact(this);
+                    handled = true;
                 }
+
+                RefreshInteractTarget(force: true);
+
+                if (handled)
+                {
+                    GetTree()?.SetInputAsHandled();
+                }
+            }
+        }
+
+        private void ApplyInteractRangeToShape()
+        {
+            if (_interactorShape?.Shape is CircleShape2D circle)
+            {
+                circle.Radius = _interactRange;
             }
         }
 
@@ -30,7 +82,7 @@ namespace SilentTestimony.Player
             {
                 if (!_nearbyInteractables.Contains(interactable))
                     _nearbyInteractables.Add(interactable);
-                UpdatePrompt();
+                RefreshInteractTarget(force: true);
             }
         }
 
@@ -39,43 +91,67 @@ namespace SilentTestimony.Player
             if (body is IInteractable interactable)
             {
                 _nearbyInteractables.Remove(interactable);
-                UpdatePrompt();
+                RefreshInteractTarget(force: true);
+            }
+        }
+
+        private void CleanupInteractables()
+        {
+            for (int i = _nearbyInteractables.Count - 1; i >= 0; i--)
+            {
+                var interactable = _nearbyInteractables[i];
+                if (interactable is not GodotObject go || !GodotObject.IsInstanceValid(go))
+                {
+                    _nearbyInteractables.RemoveAt(i);
+                }
             }
         }
 
         private IInteractable GetCurrentInteractTarget()
         {
+            CleanupInteractables();
+
             IInteractable closest = null;
             float best = float.MaxValue;
-            foreach (var it in _nearbyInteractables)
+            float rangeSq = _interactRange * _interactRange;
+
+            foreach (var interactable in _nearbyInteractables)
             {
-                if (it is Node2D n2)
+                if (interactable is not Node2D node)
+                    continue;
+
+                float distanceSq = node.GlobalPosition.DistanceSquaredTo(GlobalPosition);
+                if (distanceSq > rangeSq)
+                    continue;
+
+                if (distanceSq < best)
                 {
-                    float d = n2.GlobalPosition.DistanceTo(GlobalPosition);
-                    if (d < best)
-                    {
-                        best = d;
-                        closest = it;
-                    }
+                    best = distanceSq;
+                    closest = interactable;
                 }
             }
+
             return closest;
         }
 
-        private void UpdatePrompt()
+        private void RefreshInteractTarget(bool force = false)
         {
-            var prompt = GetNodeOrNull<InteractionPrompt>("InteractionPrompt");
-            if (prompt == null)
+            var target = GetCurrentInteractTarget();
+            if (!force && ReferenceEquals(target, _currentInteractTarget))
                 return;
 
-            var current = GetCurrentInteractTarget();
-            if (current != null)
+            _currentInteractTarget = target;
+
+            if (_interactionPrompt == null)
+                return;
+
+            if (_currentInteractTarget != null)
             {
-                prompt.ShowPrompt(current.GetInteractPrompt());
+                _interactionPrompt.ShowPrompt(_currentInteractTarget.GetInteractPrompt());
             }
             else
             {
-                prompt.HidePrompt();
+                _interactionPrompt.HidePrompt();
             }
         }
     }
